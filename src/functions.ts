ea541544ce32ea365b081d7c67f3a6a99f5b684a
LyfeWalker <nicholas.munro@maine.edu>
2026-09-14 18:51:39 -0400

    Added fahrenheit to celsius, and add 3 functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    let celsius = (temperature - 32) * (5 / 9);
+    return celsius;
 }
 
 /**
@@ -12,7 +13,11 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    first > 0 ? (sum += first) : sum;
+    second > 0 ? (sum += second) : sum;
+    third > 0 ? (sum += third) : sum;
+    return sum;
 }
 
 /**
